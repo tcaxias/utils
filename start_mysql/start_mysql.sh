7934f371b20217f483db576cf863233eb72f537e
Tiago Caxias <tiago.msilva@africainternetgroup.com>
2016-05-12 10:31:54 +0100

    Changed start_mysql from bash to sh

---
+++ start_mysql/start_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+[ -d /var/lib/mysql/mysql ] || \
+    mysql_install_db
+
+mkdir -p \
+    /var/lib/mysql \
+    /var/log/mysql \
+    /var/run/mysqld/tmp \
+    /run/mysqld
+
+chown mysql.mysql -R \
+    /var/lib/mysql \
+    /var/log/mysql \
+    /var/run/mysqld \
+    /run/mysqld
+
+exec mysqld_safe --plugin-load=server_audit.so $@
