ba1f3112b4b8a44ce58ac5ce9106275482e9f8c8
Tiago Caxias <tiago.msilva@africainternetgroup.com>
2016-05-19 17:46:34 +0100

    Changed my.cnf location

--- get_mysql_port/get_mysql_port.sh
+++ get_mysql_port/get_mysql_port.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FILE=$1
+[ -z "$FILE" ] && FILE="/etc/mysql/*/my.cnf"
 [ -z "$FILE" ] && FILE="/etc/mysql/my.cnf"
 
 exec grep -E '\s*port\s+=.*' $FILE | sed -E -e 's|.+= *([^ ]+).*|\1|g' |head -1
