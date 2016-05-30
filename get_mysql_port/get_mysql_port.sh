44f2b1857aa067de52453c2eb724e52cbd1a205c
Tiago Caxias <tiago.msilva@africainternetgroup.com>
2016-05-30 09:17:27 +0100

    Changed get_mysql_oprt fo support multi file and report default port in nothing was found in files

--- get_mysql_port/get_mysql_port.sh
+++ get_mysql_port/get_mysql_port.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-FILE=$1
-[ -z "$FILE" ] && FILE="/etc/mysql/*/my.cnf"
-[ -z "$FILE" ] && FILE="/etc/mysql/my.cnf"
+FILES=$@
+[ -z "$FILES" ] && FILE="/etc/mysql/*/my.cnf"
+[ -z "$FILES" ] && FILE="/etc/mysql/my.cnf"
 
-exec grep -E '\s*port\s+=.*' $FILE | sed -E -e 's|.+= *([^ ]+).*|\1|g' |head -1
+exec grep -E '\s*port\s+=.*' $FILES | sed -E -e 's|.+= *([^ ]+).*|\1|g' | head -1 | grep -E -e '$' || echo 3306
