d653b161d902ea0086fc9cfcd026a56154be7b75
Tiago Caxias <tiago.msilva@africainternetgroup.com>
2016-06-02 17:08:08 +0100

    Wrong variable name caused a bug on fallback for lack of parameters

--- get_mysql_port/get_mysql_port.sh
+++ get_mysql_port/get_mysql_port.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 FILES=$@
-[ -z "$FILES" ] && FILE="/etc/mysql/*/my.cnf"
-[ -z "$FILES" ] && FILE="/etc/mysql/my.cnf"
+[ -z "$FILES" ] && FILES="/etc/mysql/*/my.cnf"
+[ -z "$FILES" ] && FILES="/etc/mysql/my.cnf"
 
 exec grep -E '\s*port\s+=.*' $FILES | sed -E -e 's|.+= *([^ ]+).*|\1|g' | head -1 | grep -E -e '$' || echo 3306
