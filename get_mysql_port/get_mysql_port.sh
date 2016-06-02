3d9e5af4cd025f4f9c3aee6b5b5c9ee93bd4cf61
Tiago Caxias <tiago.msilva@africainternetgroup.com>
2016-06-02 17:25:10 +0100

    Finish fallback files method

--- get_mysql_port/get_mysql_port.sh
+++ get_mysql_port/get_mysql_port.sh
@@ -2,6 +2,6 @@
 
 FILES=$@
 [ -z "$FILES" ] && FILES="/etc/mysql/*/my.cnf"
-[ -z "$FILES" ] && FILES="/etc/mysql/my.cnf"
+[ -e "$FILES" ] || FILES="/etc/mysql/my.cnf"
 
 exec grep -E '\s*port\s+=.*' $FILES | sed -E -e 's|.+= *([^ ]+).*|\1|g' | head -1 | grep -E -e '$' || echo 3306
