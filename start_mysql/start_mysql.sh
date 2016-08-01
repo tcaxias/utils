b3aef97f979bd14001ce1d2440005e211668a32e
Tiago Caxias <tiago.msilva@africainternetgroup.com>
2016-08-01 12:16:28 +0100

    start mysql is now more reliable: tmpdir and dir permissions are better handled now

--- start_mysql/start_mysql.sh
+++ start_mysql/start_mysql.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-[ -d /var/lib/mysql/mysql/ ] || \
-    mysql_install_db
+tmpdir=$(grep tmpdir /etc/mysql/conf.d/*.cnf /etc/mysql/my.cnf | head -1 | sed -r -e 's#[^=]+=\s+(\S+)#\1#')
+[ -z tmpdir ] && tmpdir="/tmp"
 
 mkdir -p \
-    /var/lib/mysql \
+    /var/lib/mysql/tmp \
     /var/log/mysql \
     /var/run/mysqld/tmp \
-    /run/mysqld
+    /run/mysqld \
+    $tmpdir
+
+[ -d /var/lib/mysql/mysql/ ] || \
+    mysql_install_db --no-defaults
 
 chown mysql.mysql -R \
     /var/lib/mysql \
