8e57d9ee2c2b04b8b1c42701420cc84aeb97dc80
Tiago Caxias <tcx.taliesin@gmail.com>
2017-04-28 16:52:15 +0100

    Make monitor honor the ro_slave flag

--- mysql_monitor/mysql_monitor.sh
+++ mysql_monitor/mysql_monitor.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 [ -n "$MYSQL_USER" ] && MYSQL_USER=" -u $MYSQL_USER"
 [ -n "$MYSQL_PASSWD" ] && MYSQL_PASSWD=" -p$MYSQL_PASSWD"
 
 mysql="mysql $MYSQL_USER $MYSQL_PASSWD $MYOPTS"
 $mysql -Nrse"select 1" > /dev/null || { echo "no mysql access using $mysql" && sleep 30s && exit 1; }
+
+if [ "_$RECOVER_FROM" != '_' ] && [ ! -f '/var/lib/mysql/replicating_from' ]; then
+    BINLOG=$(head -1 /var/lib/mysql/xtrabackup_binlog_info|cut -f1 -d' ')
+    BINPOS=$(head -1 /var/lib/mysql/xtrabackup_binlog_info|cut -f2 -d' ')
+    MASTER=$(head -1 /var/lib/mysql/recover_from|sed -r 's|([a-z]{3,5}://)?([^/]+).*|\2|')
+    PORT=$(head -1 /var/lib/mysql/port||echo 3306)
+    CMD_AUTH="change master to MASTER_CONNECT_RETRY=10, MASTER_PASSWORD='"$REPL_PASS"', MASTER_USER='"$REPL_USER"';"
+    CMD_POS="change master to MASTER_LOG_FILE='"$BINLOG"', MASTER_LOG_POS=$BINPOS, MASTER_HOST='"$MASTER"', MASTER_PORT=$PORT;"
+    CMD_START="set global slave_parallel_threads=20; select sleep(1); start slave io_thread; select sleep(2); STOP SLAVE; CHANGE MASTER TO master_use_gtid=slave_pos; select sleep (1); START SLAVE;"
+    echo $CMD_AUTH | mysql
+    echo $CMD_POS | mysql
+    mysql -e "$CMD_START" && \
+        echo $CMD_POS | \
+        tee /var/lib/mysql/replicating_from
+fi
 
 [ -z "$TIME" ] && TIME=60
 timeout=$TIME
@@ -62,6 +77,7 @@ do
     slave=$(check_slave)
     if [ $alive -lt 0 ]; then
         echo "can't connect to DB"
+        $(stop_listen)
     elif [ "$slave" = "-1" ]; then
         state=$(check_galera)
     else
@@ -72,6 +88,7 @@ do
     if [ "0$state" -eq 4 ] || [ "0$state" -eq 9 ] || [ "0$lag" -lt "0$timeout" ]; then
         tzs=$(check_tzs)
         if [ "0$tzs" -ge $minimum_tzs ] && [ ! -f '/app/no_listen' ]; then
+            set_ro
             $(start_listen)
         else
             $(stop_listen)
--- start_mysql/start_mysql.sh
+++ start_mysql/start_mysql.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "_$RECOVER_FROM" != '_' ] && [ ! -f '/var/lib/mysql/recovered_from' ]; then
+    GZ=$(which pigz || which gzip)
     rm -rf /var/lib/mysql/* /var/log/mysql/*
-    wget -O- $RECOVER_FROM | tar -zxC /var/lib/mysql/
+    wget -O- $RECOVER_FROM | tar -I$GZ -xC /var/lib/mysql/
     echo $RECOVER_FROM > /var/lib/mysql/recovered_from
+    date >> /var/lib/mysql/recovered_from
 fi
 
 tmpdir=$(grep tmpdir /etc/mysql/conf.d/*.cnf /etc/mysql/my.cnf | head -1 | sed -r -e 's#[^=]+=\s+(\S+)#\1#')
